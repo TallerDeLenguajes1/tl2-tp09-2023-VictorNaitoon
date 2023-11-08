40b805555182e5e769e4235eff1b1e6a07969493
VictorNaitoon <victormolinalvp@gmail.com>
2023-11-08 00:22:42 -0300

    agregando los models

---
+++ Kanban/Models/Tablero.cs
@@ -0,0 +1,10 @@
+﻿namespace Kanban.Models
+{
+    public class Tablero
+    {
+        public int IdTablero { get; set; }
+        public int IdUsuarioPropietario { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ Kanban/Models/Tarea.cs
@@ -0,0 +1,21 @@
+﻿namespace Kanban.Models
+{
+    public enum EstadoTarea
+    {
+        Ideas,
+        ToDo,
+        Doing,
+        Review,
+        Done
+    }
+    public class Tarea
+    {
+        public int IdTarea { get; set; }
+        public int IdTablero { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion {  get; set; }
+        public string? Color {  get; set; }
+        public EstadoTarea Estado {  get; set; }
+        public int IdUsuarioAsignado { get; set; }
+    }
+}
